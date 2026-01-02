86a25b9d8130eab67713cbd29012618aeb0b7672
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2026-01-02 16:17:14 +0900

    feat(ErrorMessage): 이동할 칸,재시도 여부의 에러에 대한 에러메세지 작성

--- src/main/java/bridge/util/ErrorMessage.java
+++ src/main/java/bridge/util/ErrorMessage.java
@@ -5,7 +5,9 @@ public enum ErrorMessage {
     INPUT_ERROR("유효하지 않은 입력 값입니다. 다시 입력해 주세요."),
     TYPE_ERROR("타입이 불일치 합니다. 다시 입력해 주세요."),
     NUMBER_FORMAT_ERROR("숫자형식 에러"),
-    BRIDGE_LENGTH_ERROR("다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+    BRIDGE_LENGTH_ERROR("다리 길이는 3부터 20 사이의 숫자여야 합니다."),
+    MOVING_FORMAT_ERROR("이동할 칸은 U, D 둘 중 하나여야합니다. 다시 입력해 주세요."),
+    RESTART_FORMAT_ERROR("재시도 여부는 R, Q 둘 중 하나여야합니다. 다시 입력해 주세요.");
 
     private final static String PREFIX = "[ERROR] ";
     private final String message;
