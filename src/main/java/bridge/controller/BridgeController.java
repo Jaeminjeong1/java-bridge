f52919e80c101151c91ea67a43fa63cb1944b3ef
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2026-01-02 15:14:45 +0900

    feat: Application, BridgeController 연결

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -1,4 +1,17 @@
 package bridge.controller;
 
+import bridge.service.BridgeService;
+
 public class BridgeController {
+
+    private final BridgeService bridgeService;
+
+    public BridgeController(BridgeService bridgeService) {
+        this.bridgeService = bridgeService;
+    }
+
+    public void start() {
+
+    }
+
 }
